96be593465fdd697186a2b95bd953332a8925ac0
Mgithus <109915952+Mgithus@users.noreply.github.com>
2024-03-15 11:56:16 +0500

    Update dt_ut.py

--- SwinUNETR/BRATS21/UTS/dt_ut.py
+++ SwinUNETR/BRATS21/UTS/dt_ut.py
@@ -91,7 +91,7 @@ def datafold_read(datalist, basedir, fold=0, key="training"):
 
 
 
-def get_loader(args):
+def gt_ld(args):
     data_dir = args.data_dir
     datalist_json = args.json_list
     train_files, validation_files = datafold_read(datalist=datalist_json, basedir=data_dir, fold=args.fold)
