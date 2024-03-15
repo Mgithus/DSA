9ba70a22797a40a18fb697b302cae1ca17badb91
Mgithus <109915952+Mgithus@users.noreply.github.com>
2024-03-15 11:46:48 +0500

    Update train.py

--- SwinUNETR/BRATS21/train.py
+++ SwinUNETR/BRATS21/train.py
@@ -21,7 +21,7 @@ import torch.nn.parallel
 import torch.utils.data.distributed
 from OPTIM.LWP_lrscheduler import LinearWarmupCosineAnnealingLR
 from trainer import run_training
-from utils.data_utils import get_loader
+from UTS.dt_ut import get_loader
 
 from monai.inferers import sliding_window_inference
 from monai.losses import DiceLoss
